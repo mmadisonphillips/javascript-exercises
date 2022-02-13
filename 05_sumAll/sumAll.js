f947a50de12c208108d393bea507456942beb7c1
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-13 00:57:37 -0600

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(inputA, inputB) {
+    if (typeof inputA != 'number' || typeof inputB != 'number' ||
+        inputA < 0 || inputB < 0) {
+        return 'ERROR';
+    }
+    let max = Math.max(inputA, inputB);
+    let min = Math.min(inputA, inputB);
+    return (max - min + 1) * (max + min) / 2;
 };
 
 // Do not edit below this line

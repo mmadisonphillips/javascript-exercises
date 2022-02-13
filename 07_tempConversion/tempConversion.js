7f6b6ecafad20ac054e1e6f43ef93849ff078c55
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-13 03:04:46 -0600

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+    return Math.round(10 * (f - 32) * 5 / 9) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+    return Math.round(10 * (32 + c * 9 / 5)) / 10;
 };
 
 // Do not edit below this line

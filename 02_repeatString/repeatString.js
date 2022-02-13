8f93b7d3809e4768f14097b595dae13ef98ff244
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-12 20:11:28 -0600

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(inString, num) {
+    if (typeof num != 'number' || num < 0) { 
+        return 'ERROR';
+    }
+    return Array(num).fill(inString).join('');
 };
 
 // Do not edit below this line

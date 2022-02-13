15ab7b4e361158205d11974a70ccd7640bbaffaf
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-13 00:39:01 -0600

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inString) {
+    let buffer = new Array();
+    for (let character of inString) {
+        buffer.unshift(character);
+    }
+    return buffer.join('');
 };
 
 // Do not edit below this line

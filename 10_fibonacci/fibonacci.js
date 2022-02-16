3788c82e1610d538bd22cf9f6fc4ea787831f56a
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-15 19:43:22 -0600

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = parseInt(n);
+  if (typeof n != 'number' || n < 1 ) return 'OOPS';
+  let fibArray = [1,1];
+  for (let i = 2; i < n; i++) {
+    fibArray.push(fibArray[i-1] + fibArray[i-2]);
+  }
+  console.log(fibArray);
+  return fibArray[n-1];
 };
 
 // Do not edit below this line

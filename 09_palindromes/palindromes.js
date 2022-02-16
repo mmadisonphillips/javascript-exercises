c06cf639040f255475204e6fc99d3b2378fd2e80
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-15 19:35:23 -0600

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (inString) {
+  let letterArray = inString.toLowerCase().match(/[a-z]/g);
+  for (let i = 0; i < letterArray.length/2; i++) {
+    if (letterArray[i] !== letterArray[letterArray.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line

5a8328cae780f3d6162ed03192e1a0ea31c4b018
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-15 19:24:53 -0600

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(x, y) {
+  if (typeof x != 'number' || typeof y != 'number') {
+    return 'error';
+  }
+  return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  if (typeof x != 'number' || typeof y != 'number') {
+    return 'error';
+  }
+  return x-y;
 };
 
 const sum = function() {
-	
+  let total = 0;
+  for (let i of arguments[0]) {
+    if (typeof i != 'number') return 'error';
+    total += i;
+  }
+  return total;
 };
 
 const multiply = function() {
-
+  let total = 1;
+  for (let i of arguments[0]) {
+    if (typeof i != 'number') return 'error';
+    total *= i;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if (typeof x != 'number' || typeof y != 'number') {
+    return 'error';
+  }
+  return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (typeof x != 'number' || x < 0) {
+    return 'error';
+  }
+  let total = 1;
+  for (;x>0;x--) {
+    total *= x;
+  }
+  return total;
 };
 
 // Do not edit below this line

6de1e5393817ba5c2828f1577053b552a01429a2
M. Madison Phillips <michaelmadisonphillips@gmail.com>
2022-02-16 04:14:15 -0600

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personArray) {
+  function getAge(person) {
+    return age = (person.yearOfDeath || (new Date()).getFullYear()) - person.yearOfBirth;
+  }
+  return personArray.reduce((oldestPerson, person) => {
+    return getAge(person) > getAge(oldestPerson) ? person : oldestPerson;
+  })
 };
 
 // Do not edit below this line
